74b5adf410c3e7696fecd5ea1ebe5106113a34f3
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2026-02-18 16:58:25 -0800

    Complete dom manip exercise

---
+++ js-scratch/dom-manipulation/dom_manip.js
@@ -0,0 +1,37 @@
+const container = document.querySelector("#container");
+
+const paragraph = document.createElement("p");
+
+paragraph.textContent = "Hey I'm red!";
+
+paragraph.style.color = "red";
+
+container.appendChild(paragraph);
+
+const blueHeading = document.createElement("h3");
+
+blueHeading.textContent = "I'm a blue h3!"
+
+blueHeading.style.color = "blue";
+
+container.appendChild(blueHeading);
+
+const container2 = document.createElement("div");
+
+container2.style.border = "black 1px solid";
+
+container2.style.backgroundColor = "pink";
+
+const nestedHeading = document.createElement("h1");
+
+nestedHeading.textContent = "I'm in a div";
+
+const nestedP = document.createElement("p");
+
+nestedP.textContent = "ME TOO!";
+
+container2.appendChild(nestedHeading);
+
+container2.appendChild(nestedP);
+
+container.appendChild(container2);
