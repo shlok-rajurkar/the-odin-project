5def2d8c530cae4fca862b71c0095665de1ef913
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2026-02-18 16:59:19 -0800

    Rearrange files

--- js-scratch/loops-and-arrays/loops-and-arrays.js
+++ js-scratch/loops-and-arrays/loops-and-arrays.js
@@ -1,6 +1,35 @@
 function sumOfTripledEvens(inputArray) {
     let evens = inputArray.filter((item) => {
-        item % 2 === 0;
+        return item % 2 === 0;
     })
-    let sum = evens.reduce()
+    let tripled = evens.map((item) => item * 3)
+
+    let sum = tripled.reduce((total, currentItem) => {
+        return total + currentItem;
+    }, 0);
+    return sum;
 }
+
+input = [1, 2, 3, 4, 5]
+
+// console.log(sumOfTripledEvens(input));
+
+function sumArray(array) {
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result;
+}
+
+function getMaxSubSum(inputArray) {
+    let result = 0;
+    for (let i = 0; i < inputArray.length; i++) {
+        for (let j = i+1; j < inputArray.length; j++) {
+        candidates.push(sumArray(inputArray.slice(i, j)))
+        }
+    }
+    return result;
+}
+
+console.log(getMaxSubSum([1, 2, 3]));
